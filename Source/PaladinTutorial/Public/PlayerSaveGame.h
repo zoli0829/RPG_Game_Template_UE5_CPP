5c9cfd5f75e6f6e777fc0242564f6efcf939552d
Zoltan Vegh <zoli0829@gmail.com>
2025-12-09 18:05:37 +0000

    Saving Game with C++
    Create Player Save Game Class

---
+++ Source/PaladinTutorial/Public/PlayerSaveGame.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "PlayerSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PALADINTUTORIAL_API UPlayerSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere, Category="SaveGame")
+	float Health;
+};
